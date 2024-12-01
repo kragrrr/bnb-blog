2d0cf337083fb0622e458d061caca638fe71e2f1
Krish Agrawal <38100966+kragrrr@users.noreply.github.com>
2024-12-01 13:57:41 +1100

    New Blog Post on 2024-12-01 13:57:41

--- images.py
+++ images.py
@@ -15,15 +15,15 @@ for filename in os.listdir(posts_dir):
         with open(filepath, "r") as file:
             content = file.read()
         
-        # Step 2: Find all image links in the format ![Image Description](/images/Pasted%20image%20...%20.png)
-        images = re.findall(r'!\[.*?\]\(/images/([^)]*\.png)\)', content)
+        # Step 2: Find all image links in the format ![[image.png]]
+        images = re.findall(r"\!\[\[(.*?)\]\]", content)
         
         # Step 3: Replace image links and ensure URLs are correctly formatted
         for image in images:
 
             # Prepare the Markdown-compatible link with %20 replacing spaces
-            markdown_image = f"![Image Description](/images/{image.replace(' ', '%20')})"
-            content = content.replace(f"[[{image}]]", markdown_image)
+            image_link = image.replace(" ", "%20")
+            content = content.replace(f"![[{image}]]", f"![](/images/{image_link})")
             
             # Step 4: Copy the image to the Hugo static/images directory if it exists
             image_source = os.path.join(attachments_dir, image)
